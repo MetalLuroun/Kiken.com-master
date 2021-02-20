73b875d7a030d63fad8892136568a6a7b65e8946
Haoran_H <48746182+MetalLuroun@users.noreply.github.com>
2021-02-20 18:17:02 +0800

    2021

--- src/getImg.js
+++ src/getImg.js
@@ -1,4 +1,4 @@
-async function getImg(){
+async function getWorkImg(){
     let requestOptions = {
         method: 'GET',
         redirect: 'follow',
@@ -16,6 +16,26 @@ async function getImg(){
     })
     .catch(error => console.log('error', error));
 }
+
+async function getPGImg(){
+    let requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+    };
+    let section = document.getElementById('mainSection')
+    await fetch("http://146.56.199.136:8080/api/picture?page=0&page_size=100", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            for (let i of result["images"]){
+                console.log(i)
+                let img = document.createElement("img")
+                img.src = "http://" + i['Url']
+                section.appendChild(img)
+            }
+        })
+        .catch(error => console.log('error', error));
+}
+
 
 function showFile(){
     
