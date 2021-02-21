e3d0661ada334e054b4f27846b9883d3b4eca5c4
MetalLuroun <MetalLuroun@users.noreply.github.com>
2021-02-21 11:39:09 +0800

    to LJZ

--- src/getImg.js
+++ src/getImg.js
@@ -36,28 +36,54 @@ async function getPGImg(){
         .catch(error => console.log('error', error));
 }
 
-
-function showFile(){
-    
-}
-
-async function uploadImg(){
-    let imgs = document.getElementById("upload")
-    let formdata = new FormData();
-    formdata.append("token", "123456");
-    formdata.append("tag", "test");
-    formdata.append("file", imgs.files[0])
-    
+async function getPGImgOnload(){
     let requestOptions = {
-        method: 'POST',
-        body: formdata,
-        redirect: 'follow'
-      };
-
-    fetch("http://146.56.199.136:8080/api/picture", requestOptions)
-    .then(response => response.json())
-    .catch(error => console.log("error" , error))
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-
+        method: 'GET',
+        redirect: 'follow',
+    };
+    let section = document.getElementById('pgimglist')
+    await fetch("http://146.56.199.136:8080/api/picture?page=0&page_size=100tag=playground", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            for (let i of result["images"]){
+                console.log(i)
+                let img = document.createElement("img")
+                img.src = "http://" + i['Url']
+                section.appendChild(img)
+            }
+        })
+        .catch(error => console.log('error', error));
 }
+
+async function getWorkImgOnload(){
+    let requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+    };
+    let section = document.getElementById('workimglist')
+    await fetch("http://146.56.199.136:8080/api/picture?page=0&page_size=100", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            for (let i of result["images"]){
+                console.log(i)
+                let img = document.createElement("img")
+                img.src = "http://" + i['Url']
+                section.appendChild(img)
+            }
+        })
+        .catch(error => console.log('error', error));
+}
+
+
+function upload(event) {
+    const formData = new FormData();
+    formData.append("token", "123456");
+    formData.append('file', event.target.files[0]);
+    fetch('http://146.56.199.136:8080/api/picture', {
+      method: 'post',
+      body: formData
+      }).then(response => response.json())
+      .then((data) => {
+           console.log(data);
+      });
+     }
